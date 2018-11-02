56898e1d3643f001f7612c724fb83a07d63a7db0
Grzegorz Ilnicki <g.ilnicki@wincan.com>
2018-11-02 15:00:25 +0100

    Why the last test crashes?

--- OverlappingDetector_test.cc
+++ OverlappingDetector_test.cc
@@ -105,22 +105,102 @@ TEST(OverlappingDetector, OverlappingsReturnsCorrectIdsForTwoPointsAtTheSamePosi
 
     OverlappingDetector od(m);    
     
-    OverlappingDetector::Overlappings overlappings_map = od.overlappings();
-    auto overlappingIt = overlappings_map.find(m[firstId]);
-    auto idsSet = (*overlappingIt).second;
-    ASSERT_FALSE(idsSet.find(firstId) == idsSet.end());
-    ASSERT_FALSE(idsSet.find(secondId) == idsSet.end());
+    auto idsSet = (*od.overlappings().find(m[firstId])).second;
+    ASSERT_TRUE(idsSet.find(firstId) != idsSet.end());
+    ASSERT_TRUE(idsSet.find(secondId) != idsSet.end());
 }
 
-// TEST(OverlappingDetector, OverlappingsReturnsAllCorrectPositionsForMultiplePointsAtTheSamePosition)
-// {
-//     OverlappingDetector::RectIdMap m;
-//     m[0] = {1,2,1,2};
-//     m[1] = m[0];
-//     OverlappingDetector od(m);    
-//     
-//     ASSERT_EQ(od.overlappings().back().first, m[0]);
-// }
+TEST(OverlappingDetector, OverlappingsReturnsCorrectIdsForTwoPointsAtTheSamePositionAndCanCallItMultipleTimes)
+{
+    OverlappingDetector::RectIdMap m;
+    
+    int firstId = 77;
+    int secondId = 99;
+    m[firstId] = {1,2,1,2};
+    m[secondId] = m[firstId];
 
+    OverlappingDetector od(m);    
+    
+    ASSERT_TRUE((*od.overlappings().find(m[firstId])).second.find(firstId) != (*od.overlappings().find(m[firstId])).second.end());
+    ASSERT_TRUE((*od.overlappings().find(m[firstId])).second.find(secondId) != (*od.overlappings().find(m[firstId])).second.end());
+}
 
+TEST(OverlappingDetector, OverlappingsReturnsCorrectIdsForThreePointsAtTheSamePosition)
+{
+    OverlappingDetector::RectIdMap m;
+    
+    int firstId = 77;
+    int secondId = 99;
+    int thirdId = 888;
+    m[firstId] = {1,2,1,2};
+    m[secondId] = m[firstId];
+    m[thirdId] = m[firstId];
 
+    OverlappingDetector od(m);    
+    
+    auto idsSet = (*od.overlappings().find(m[firstId])).second;
+    ASSERT_TRUE(idsSet.find(firstId) != idsSet.end());
+    ASSERT_TRUE(idsSet.find(secondId) != idsSet.end());
+    ASSERT_TRUE(idsSet.find(thirdId) != idsSet.end());
+}
+
+TEST(OverlappingDetector, OverlappingsReturnsCorrectIdsForTwoPointsAtTheSamePositionAndNotForThirdAtWrongPosition)
+{
+    OverlappingDetector::RectIdMap m;
+    
+    int firstId = 77;
+    int secondId = 99;
+    int thirdId = 888;
+    m[firstId] = {1,2,1,2};
+    m[secondId] = m[firstId];
+    m[thirdId] = {2,3,2,3};
+
+    OverlappingDetector od(m);    
+    
+    auto idsSet = (*od.overlappings().find(m[firstId])).second;
+    ASSERT_TRUE(idsSet.find(firstId) != idsSet.end());
+    ASSERT_TRUE(idsSet.find(secondId) != idsSet.end());
+    ASSERT_FALSE(idsSet.find(thirdId) != idsSet.end());
+}
+
+TEST(OverlappingDetector, OverlappingsReturnsCorrectRectangleForTwoSimplestIdenticalRectangles)
+{
+    OverlappingDetector::RectIdMap m;
+    
+    int firstId = 77;
+    int secondId = 99;
+    m[firstId] = {2,2,2,3};
+    m[secondId] = m[firstId];
+
+    OverlappingDetector od(m);    
+    
+    auto overlappingFound = (*od.overlappings().find(m[firstId]));
+    auto rect = overlappingFound.first;
+    auto idsSet = overlappingFound.second;
+    ASSERT_TRUE(idsSet.find(firstId) != idsSet.end());
+    ASSERT_TRUE(idsSet.find(secondId) != idsSet.end());
+    ASSERT_EQ(rect, m[firstId]);
+}
+
+TEST(OverlappingDetector, OverlappingsReturnsCorrectRectangleForTwoSimplestOverlappingRectangles)
+{
+    OverlappingDetector::RectIdMap m;
+    
+    int firstId = 77;
+    int secondId = 99;
+    m[firstId] = {2,2,2,3};
+    m[secondId] = {2,3,2,4};
+    
+    OverlappingDetector::IntRect expectedResult = {2,3,2,3};
+
+    OverlappingDetector od(m);    
+    
+    auto overlappingFound = (*od.overlappings().find(m[firstId]));
+    OverlappingDetector::IntRect rect = overlappingFound.first;
+    auto idsSet = overlappingFound.second;
+    
+    ASSERT_TRUE(idsSet.find(firstId) != idsSet.end());
+    ASSERT_TRUE(idsSet.find(secondId) != idsSet.end());
+    
+//     ASSERT_EQ(rect, expectedResult);
+}
